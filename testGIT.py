335ed2cf4d52a704a4c23a5dafcc679f4116bbfd
曹瑞芳 <5198032+caoruifang@user.noreply.gitee.com>
2024-05-29 20:18:42 +0800

    低次提交

---
+++ testGIT.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/5/29 20:14
+# @Author  : caoruifang
+# @File    : testGIT.py
+# @Software: PyCharm
+"""
+测试python中git的用法
+"""
+print("1")
+
+print("2")
+
+print("2")
+
